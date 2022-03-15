a7aaccca6a73662207e2e72a239e1b5e02486f26
theofabilous <92238946+theofabilous@users.noreply.github.com>
2022-03-15 02:23:15 -0400

    Biterator.h updates (context tree)

--- src/biterator.h
+++ src/biterator.h
@@ -23,6 +23,8 @@ struct _BitArray;
 
 size_t bitarray_slice(struct _BitArray *self, size_t i, size_t j);
 
+uint8_t bitarray_get(struct _BitArray *self, size_t i);
+
 /* --------------------- */
 
 
@@ -52,17 +54,154 @@ typedef union _Function
 	void (*void_iter_f)(struct _Biterator*);
 } Function;
 
+// To impl
+typedef struct _BiteratorTemplate
+{
+	void **data;
+} BiteratorTemplate;
 
+
+// add some useful flags:
+// 1) FUNCTION_WAS_ALLOCATED
+// 2) ...
 typedef struct _Biterator
 {
 	size_t increment;
 	size_t curr;
-	long long int max;
+	size_t max;
 	uint8_t sig;
 	Function *function;
 	struct _BitArray *parent;
 	size_t (*read)(struct _Biterator* self, size_t n);
+	size_t (*skip)(struct _Biterator* self, size_t n);
+	size_t (*peek)(struct _Biterator* self, size_t n);
+	void *user_data;
 } Biterator;
+
+
+static inline bool biterator_finished(Biterator* self)
+{
+	return self->curr < self->max;
+}
+
+static inline bool biterator_ok(Biterator* self)
+{
+	return !(self->curr < self->max) && !(self->parent == NULL);
+}
+
+bool biterator_read_bit(Biterator *it)
+{
+	// handle errors needed....
+	if(biterator_finished(it))
+		return false;
+
+	// prob could optimize
+	bool read = bitarray_get(it->parent, it->curr);
+	it->curr++;
+	return read;
+}
+
+bool biterator_expect_bit(Biterator* self, bool bit)
+{
+	switch(bit | (biterator_read_bit(self) << 1))
+	{
+		case 0b10:
+		case 0b01:
+			self->curr--;
+			return false;
+		default:
+			return true;
+	}
+}
+
+size_t biterator_read(Biterator *it, size_t n)
+{
+	if(it->parent == NULL)
+		return -1;
+	size_t diff = it->curr - it->max;
+	size_t ret;
+	if(diff > 0 && diff < it->increment)
+	{
+		ret = bitarray_slice(it->parent, it->curr, it->curr + diff);
+		it->curr += diff;
+		return ret;
+	}
+	else if(diff > 0)
+	{
+		ret = bitarray_slice(it->parent, it->curr, it->curr + it->increment);
+		it->curr += it->increment;
+		return ret;
+	}
+	else
+		return -1;
+}
+
+// Could possibly be optimized by adding a single bit flag
+// for if its a leaf, and then another for left(0), right(1)
+// which would make switch case more direct (? i think)
+/*
+FLAGS: 0 0 0 0 0 0 0 0
+----------------------
+    &  0 0 0 0 0 0 0 1         The bit **currently unused**
+    &  0 0 0 0 0 0 1 0         Right child?
+    &  0 0 0 0 0 1 0 0         Left child?
+    &  0 0 0 0 0 1 1 0         (Is a leaf?)
+    &  0 0 0 0 1 0 0 0         ? Has defined fallback : uses default fallback
+	!!! ADD A FLAG FOR ITERATOR CHILDREN (?)
+	!!! ADD FLAG(S) FOR INT <-> COMMON ITERATOR EFFECTS (ex.: read certain size, skip...)
+	.... todo
+*/
+typedef struct _ContextNode
+{
+	uint8_t flags;
+	union _data {
+		struct _ContextNode* children[2];
+		Biterator iters[2];
+		void (*funcs[2])(void);
+	} data;
+	
+} ContextNode;
+
+typedef struct _ContextTreeNav
+{
+	ContextNode* curr;
+	uint8_t max_height;
+	size_t accumulator;
+} ContextTreeNav;
+
+// Assumes biterator reads one bit at a time
+// Could be generalized easily by adding a field in node struct
+// and checking if the read corresponds to either
+void parse_tree(Biterator* itr, ContextNode* node, void (*fallback)(void))
+{
+	// 	if(node == NULL || biterator_finished(itr) ||
+	//	biterator_read(itr, 1) != node->flags & 1)
+	if(node == NULL || biterator_finished(itr))
+		return fallback();
+
+	// i.e. by default if both children null,
+	// fallback should be at 0
+	bool bit = 0;
+	// break needed when func called? idk
+	switch(node->flags & 0b110)
+	{
+		case 0b110:
+			parse_tree(itr, node->
+			data.children[biterator_read_bit(itr)], fallback);
+		case 0b010:
+			bit = 1;
+		case 0b100:
+			if(biterator_expect_bit(itr, bit))
+				parse_tree(itr, node->data.children[bit], fallback);
+			else
+				bit = !bit;
+		case 0b000:
+			if(node->flags & 0b1000)
+				(*(node->data.funcs[bit]))();
+			else
+				fallback();
+	}
+}
 
 
 Function *new_Function(void *f)
@@ -87,7 +226,7 @@ void del_Function(Function *func)
 }
 
 
-void reset_Biterator(Biterator *it)
+void biterator_reset(Biterator *it)
 {
 	it->curr = 0;
 }
@@ -101,46 +240,17 @@ void detach_Biterator(Biterator *it)
 }
 
 
-size_t read_Biterator(Biterator *it, size_t n)
-{
-	if(it->parent == NULL)
-	{
-		printf("Cannot read from biterator without parent array\n");
-		return -1;
-	}
-	long int diff = it->curr - it->max;
-	size_t ret;
-	if(diff > 0 && diff < it->increment)
-	{
-		ret = bitarray_slice(it->parent, it->curr, it->curr + diff);
-		it->curr += diff;
-		return ret;
-	}
-	else if(diff > 0)
-	{
-		ret = bitarray_slice(it->parent, it->curr, it->curr + it->increment);
-		it->curr += it->increment;
-		return ret;
-	}
-	else
-		return -1;
-}
 
-
-Biterator *new_Biterator(int increment, long long int max, uint8_t sig, void* f)
+Biterator *new_Biterator(int increment, size_t max, uint8_t sig, void* f)
 {
 	Biterator *it = (Biterator *) malloc(sizeof(Biterator));
 	if(it == NULL)
-	{
-		printf("Memory could not be allocated\n");
-		exit(1);
-	}
+		return NULL;
 	Function *func = new_Function(f);
 	if(func == NULL)
 	{
-		printf("Memory could not be allocated\n");
 		free(it);
-		exit(1);
+		return NULL;
 	}
 	it->increment = increment;
 	it->max = max;
@@ -148,11 +258,56 @@ Biterator *new_Biterator(int increment, long long int max, uint8_t sig, void* f)
 	it->sig = sig;
 	it->function = func;
 	it->parent = NULL;
-	it->read = read_Biterator;
+	it->read = biterator_read;
+	it->skip = NULL;
+	it->peek = NULL;
 	return it;
 }
 
+bool init_Biterator(Biterator* it, int increment, size_t max, uint8_t sig, void* f)
+{
+	Function *func = new_Function(f);
+	if(func == NULL)
+		return false;
+	it->increment = increment;
+	it->max = max;
+	it->curr = 0;
+	it->sig = sig;
+	it->function = func;
+	it->parent = NULL;
+	it->read = biterator_read;
+	it->skip = NULL;
+	it->peek = NULL;
+	return true;
+}
 
+// Prob could do use init_Function instead
+// of reallocating and (re)set function_was_alloc flag
+// accordingly
+bool replace_Biterator(Biterator* it, int increment, size_t max, uint8_t sig, void* f)
+{
+	// If was alloced, could keep allocation
+	// and read into pointer (i.e. no deletion)
+	del_Function(it->function);
+	it->function = new_Function(f);
+	if(it->function == NULL)
+		return false;
+
+	// If wasn't, ..?
+	
+	it->increment = increment;
+	it->max = max;
+	it->curr = 0;
+	it->sig = sig;
+	it->parent = NULL;
+	it->read = biterator_read;
+	it->skip = NULL;
+	it->peek = NULL;
+	return true;
+}
+
+
+// Use & impl function_was_allocated flag instead
 void del_Biterator(Biterator* it, bool delete_function)
 {
 	if(delete_function)
